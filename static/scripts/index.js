d624527c8ad2385027ffacaf44f88c424f4731a1
mjontop <mjontop0602@gmail.com>
2019-09-12 14:04:52 +0530

    menu button working on resizing screen

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -3,3 +3,14 @@ $(document).ready(() => {
         $(".tohide").slideToggle("slow")
     })
 })
+
+let getmenuback = () => {
+    if ($(window).width() > 768) {
+		$(".tohide").slideDown("fast")
+	}
+	else{
+		$(".tohide").slideUp("fast")
+	}
+}
+
+$(window).on("resize", getmenuback)
