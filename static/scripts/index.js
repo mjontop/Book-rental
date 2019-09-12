d779533fdb0eaab012509047f41207b06004a200
mjontop <mjontop0602@gmail.com>
2019-09-12 13:49:20 +0530

    menu button issue raised

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,11 +1,5 @@
-function showMenu() {
-	if (document.getElementsByClassName("tohide")[0].style.display == "none") {
-		document.getElementsByClassName("tohide")[0].style.display = "inline";
-		document.getElementsByClassName("tohide")[1].style.display = "inline";
-		document.getElementsByClassName("tohide")[2].style.display = "inline";
-	} else {
-		document.getElementsByClassName("tohide")[0].style.display = "none";
-		document.getElementsByClassName("tohide")[1].style.display = "none";
-		document.getElementsByClassName("tohide")[2].style.display = "none";
-	}
-}
+$(document).ready(() => {
+    $("#menu").click(() => {
+        $(".tohide").slideToggle("slow")
+    })
+})
